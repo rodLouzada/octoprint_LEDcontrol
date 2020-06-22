5fa5c792d8a4be3c22fe5c8624164d0db7e4745d
Rodrigo Louzada Campos <rodrigolouzada1@gmail.com>
2020-06-21 22:34:55 -0400

    Update __init__.py

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -2,16 +2,19 @@
 from __future__ import absolute_import
 
 import octoprint.plugin
+from gpiozero import LED
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.SettingsPlugin,
                        octoprint.plugin.AssetPlugin):
 	def on_after_startup(self):
-		self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+		self._logger.info("Hello World! (more: %s)" )#% self._settings.get(["url"]))
 
 	def get_settings_defaults(self):
-		return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+		#return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+        led = LED(17)
+        led.toggle()
 
 	def get_template_configs(self):
 		return [
